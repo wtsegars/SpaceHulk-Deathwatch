493a7c127ba8e580898b66b30fe9c40a06a35bcc
Darkangel <wtsegars@mac.com>
2019-10-15 17:34:38 -0400

    Added all playable chapters

--- game.py
+++ game.py
@@ -64,3 +64,34 @@ class SquadSelect(Scene):
                 be the squad's sergent while the rest are standard marines. Choose
                 wisely because some chapters will have advantages over others.
                 """))
+
+        chapters = [
+            "Ultramarines", 
+            "Space Wolves", 
+            "White Scars",
+            "Blood Angels",
+            "Salamanders",
+            "Dark Angels",
+            "Imperial Fists",
+            "Iron Hands",
+            "Raven Guard",
+            "Crimson Fists",
+            "Blood Ravens",
+            "Howling Griffons",
+            "Black Templars",
+            "Lementers",
+            "Novamarines",
+            "Carcarodons",
+            "Brazen Claws",
+            "Death Eagles",
+            "Red Talons",
+            "Angels of Vengence",
+            "Angels of Absolution",
+            "Flesh Tearers",
+            "Storm Giants",
+            "Storm Lords",
+            "Storm Wardens",
+            "Raptors",
+            "Silver Skulls",
+            "Iron Snakes",
+            "Mantis Warriors"]
