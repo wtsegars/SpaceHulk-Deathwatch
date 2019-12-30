565ff4eae8615854330356711483130c1c101d67
Darkangel <wtsegars@mac.com>
2019-12-29 22:16:13 -0500

    Worked on line of sight file

--- linesight.py
+++ linesight.py
@@ -4,6 +4,66 @@ line_of_sight = {
         "s2",
         "s3",
         "s4",
-        "s5"
+        "s5",
+        "c1"
+    },
+    "lower hall": {
+        "g3",
+        "g4",
+        "ll5",
+        "ll4",
+        "ll3",
+        "ll2",
+        "ll1",
+        "c1",
+        "lr1",
+        "lr2",
+        "lr3",
+        "lr4",
+        "lr5",
+        "g8",
+        "g7"
+    },
+    "lower left genestealer entrance": {
+        "g3",
+        "g2",
+        "g1"
+    },
+    "lower right genestealer entrance": {
+        "g7",
+        "g6",
+        "g5"
+    },
+    "left hallway": {
+        "ll5",
+        "l1",
+        "l2",
+        "l3",
+        "l4",
+        "l5",
+        "l6",
+        "l7",
+        "l8",
+        "l9",
+        "l10",
+        "l11",
+        "l12",
+        "l13"
+    },
+    "right hallway": {
+        "lr5",
+        "r1",
+        "r2",
+        "r3",
+        "r4",
+        "r5",
+        "r6",
+        "r7",
+        "r8",
+        "r9",
+        "r10",
+        "r11",
+        "r12",
+        "r13"
     }
 }
