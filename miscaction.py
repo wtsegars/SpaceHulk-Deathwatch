a7e5e843756a29b8ee7fde5f2f350f8643bcf7c8
Darkangel <wtsegars@mac.com>
2020-01-21 17:13:14 -0500

    Added more functionality to misc action

--- miscaction.py
+++ miscaction.py
@@ -1,4 +1,5 @@
 from textwrap import dedent
+import game
 
 def other_action():
     print("What would you like to do?")
@@ -6,5 +7,22 @@ def other_action():
             Reload,
             Toggle Door,
             Overwatch,
-            Clear Jam
+            Clear Jam,
+            Cancel
             """))
+
+    action_choice = input("> ")
+
+    if action_choice == "Reload":
+        reload()
+    elif action_choice == "Toggle Door":
+        toggle_door()
+    elif action_choice == "Overwatch":
+        overwatch()
+    elif action_choice == "Clear Jam":
+        clear_jam()
+    elif action_choice == "Cancel":
+        game.SpaceMarineTurn.enter.turn_menu()
+    else:
+        print("You have entered an invalid command, please try again.")
+        other_action()
