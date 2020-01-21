f8b5d9bd5ff5667f8a43b073595c547586a3c3c7
Darkangel <wtsegars@mac.com>
2020-01-21 16:57:36 -0500

    Started work on misc. action functionality

--- miscaction.py
+++ miscaction.py
@@ -1,1 +1,10 @@
+from textwrap import dedent
+
 def other_action():
+    print("What would you like to do?")
+    print(dedent("""
+            Reload,
+            Toggle Door,
+            Overwatch,
+            Clear Jam
+            """))
