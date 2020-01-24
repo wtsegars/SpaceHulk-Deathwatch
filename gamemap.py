a0494865e57e80052ce2e27a561d472c8ee282d0
Darkangel <wtsegars@mac.com>
2020-01-24 16:51:10 -0500

    Started work on the map

--- gamemap.py
+++ gamemap.py
@@ -2,5 +2,22 @@ import textwrap
 
 def map():
     print(dedent"""
-    
+
+
+
+
+
+
+                           [l6 ][cl1][cl2][cl3][cl4][c4][cr4][cr3][cr2][cr1][ r6]
+                           [l5 ]                     [c3]                   [ r5]
+                           [l4 ]            [g9][g10][c2][g12][g11]         [ r4]
+                           [l3 ]             ||                 ||          [ r3]
+                           [l2 ]                                            [ r2]
+                           [l1 ]                                            [ r1]
+                   [g3][g4][ll5][ll4][ll3][ll2][ll1][c1][lr1][lr2][lr3][lr4][lr5][g8][g7]
+                   [g2]                             [s5]                             [g6]
+                   [g1]                             [s4]                             [g5]
+                    ||                              [s3]                              ||
+                                                    [s2]
+                                                    [s1]
     """)
