454127035b3dd5ae916cdd3d6466ededa329d4ae
Darkangel <wtsegars@mac.com>
2020-02-05 15:59:03 -0500

    Added more functionality to blip deployment

--- genestealermove.py
+++ genestealermove.py
@@ -7,3 +7,6 @@ import linesight
 def genestealer_movement(a, b, c, d):
     for x in a:
         while a[x]["action_points"] > 0:
+            for y in d:
+                for z in d[y]:
+                    
