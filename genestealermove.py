427b28dc593043ef120958a33dc0b5138c7fcccc
Darkangel <wtsegars@mac.com>
2021-02-23 20:06:49 -0500

    Redoing genestealer movement

--- genestealermove.py
+++ genestealermove.py
@@ -11,13 +11,7 @@ import squad
 #d = linesight.line_of_sight
 #e = squad.squad
 
-def genestealer_movement(a, b, c, d, e):
+def genestealer_movement(a, d, e):
     for x in a: #looping through radar blips
         while a[x]["action_points"] > 0:
-            for y in d: #looping through line of sight to locate radar blips
-                for z in d[y]: #looping through specicic tiles in each individual line of sight
-                    if d[y][z] == a[x]["current_place"]:
-                        for w in e: #looping through terminator squad
-                            for v in d[y]: #looping through each individual tile in line of sight
-                                if e[w]["current_place"] == d[y][v]:
-                                    radarblips.blip_reveal(a[x], e[w]["direction"])
+            
