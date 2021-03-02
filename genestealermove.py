d404c564819be5b5dc736000d8a9806ec28e317b
Darkangel <wtsegars@mac.com>
2021-03-01 20:08:38 -0500

    Added more functionality to genestealer movement

--- genestealermove.py
+++ genestealermove.py
@@ -11,8 +11,13 @@ import squad
 #d = linesight.line_of_sight
 #e = squad.squad
 
-def genestealer_movement(a, d, e):
-    for x in a: #looping through radar blips
-        while a[x]["action points"] > 0:
-            if (a[x]["current location"]):
-                
+def genestealer_movement():
+    for x in radarblips: #looping through radar blips
+        while radarblips[x]["action points"] > 0:
+            if (radarblips[x]["current location"]):
+                for y in squad:
+                    if (squad[y]["current_place"]):
+                        for z in linesight:
+                            for a in linesight[z]:
+                                if (squad[y]["current_place"] == linesight[z][a]):
+                                    
