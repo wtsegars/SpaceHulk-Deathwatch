32d1fa45da287076db2ec21b4bd55076f4594556
Darkangel <wtsegars@mac.com>
2021-03-03 20:09:54 -0500

    Added more functionality to genestealer movement

--- genestealermove.py
+++ genestealermove.py
@@ -29,5 +29,14 @@ def genestealer_movement():
                     if (squad[y]["current_place"]):
                         for z in linesight:
                             for a in linesight[z]:
-                                if (squad[y]["current_place"] == linesight[z][a]):
-                                    tracker1 == gametiles(squad[y]["current_place"])
+                                if (radarblips[x]["current location"] == linesight[z][a]):
+                                    tracker1 == gametiles(radarblips[x]["current location"])["connected to"][0]
+                                    if (gametiles(radarblips[x]["current location"])["connected to"][1]):
+                                        tracker2 == gametiles(radarblips[x]["current location"])["connected to"][1]
+                                    if (gametiles(radarblips[x]["current location"])["connected to"][2]):
+                                        tracker3 == gametiles(radarblips[x]["current location"])["connected to"][2]
+                                    if (gametiles(radarblips[x]["current location"])["connected to"][3]):
+                                        tracker4 == gametiles(radarblips[x]["current location"])["connected to"][3]                                    
+
+                                    while (tracker1 != squad[y]["current_place"] or tracker2 != squad[y]["current_place"] or tracker3 != squad[y]["current_place"] or tracker4 != squad[y]["current_place"]):
+                                        tracker1 == gametiles(radarblips[x]["current location"])
