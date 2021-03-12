4997bb7db5374f4c8d6d2fa79a72f4c474202fc4
Darkangel <wtsegars@mac.com>
2021-03-11 20:04:00 -0500

    Added more functionality to blip reveal

--- radarblips.py
+++ radarblips.py
@@ -92,10 +92,23 @@ def blip_reveal(a, b):
             new_genestealer.clear()
             genestealer_count += 1
 
-def check_for_reveal():
+def check_for_reveal(blip):
     for c in linesight:
         for d in linesight[c]:
             if gametiles(linesight[c][d])["occupied"] == True:
                 for e in squad:
                     if squad[e]["current_place"] == gametiles(linesight[c][d]):
-                        
+                        if linesight[c] == linesight.line_of_sight[0] or linesight[c] == linesight.line_of_sight[2] or linesight[c] == linesight.line_of_sight[3] or linesight[c] == linesight.line_of_sight[4] or linesight[c] == linesight.line_of_sight[5] or linesight[c] == linesight.line_of_sight[7] or linesight[c] == linesight.line_of_sight[10]:
+                            if squad[e]["direction"] == "north":
+                                sm_index = linesight[c].index(squad[e]["current_place"])
+                                for f in range(d, sm_index, 1):
+                                    if linesight[c][f] == blip["current location"]:
+                                        blip_reveal(blip)
+                            if squad[e]["direction"] == "south":
+                                sm_index = linesight[c].index(squad[e]["current_place"])
+                                for g in range(d, sm_index, -1):
+                                    if linesight[c][g] == blip["current location"]:
+                                        blip_reveal(blip)
+                        elif linesight[c] == linesight.line_of_sight[1] or linesight[c] == linesight.line_of_sight[6] or linesight[c] == linesight.line_of_sight[8] or linesight[c] == linesight.line_of_sight[9] or linesight[c] == linesight.line_of_sight[11] or linesight[c] == linesight.line_of_sight[12] or linesight[c] == linesight.line_of_sight[13]:
+                            if squad[e]["direction"] == "west":
+                                sm_index = linesight[c].index(squad[e]["current_place"])
