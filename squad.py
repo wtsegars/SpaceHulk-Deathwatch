b6a6766704a89006ff801061f99a9801ca85b1ef
Darkangel <wtsegars@mac.com>
2021-06-29 20:08:39 -0400

    Updated the squad dict to contain more values

--- squad.py
+++ squad.py
@@ -1,17 +1,27 @@
 squad = {
     "Sergent" : {
-        "action points": 4
+        "action points": 4,
+        "overwatch": False,
+        "current_place": None
     },
     "Terminator1" : {
-        "action points": 4
+        "action points": 4,
+        "overwatch": False,
+        "current_place": None
     },
     "Terminator2" : {
-        "action points": 4
+        "action points": 4,
+        "overwatch": False,
+        "current_place": None
     },
     "Terminator3" : {
-        "action points": 4
+        "action points": 4,
+        "overwatch": False,
+        "current_place": None
     },
     "Terminator4" : {
-        "action points": 4
+        "action points": 4,
+        "overwatch": False,
+        "current_place": None
     }
 }
