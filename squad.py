49b51af55e7fabfcddd71373328918712510bacc
wtsegars <wtsegars@mac.com>
2021-08-30 20:05:28 -0400

    Added alive to each member of squad dictionary

--- squad.py
+++ squad.py
@@ -4,34 +4,39 @@ squad = {
         "overwatch": False,
         "current_place": None,
         "chapter": None,
-        "weapon loadout": None
+        "weapon loadout": None,
+        "alive": True
     },
     "Terminator1" : {
         "action points": 4,
         "overwatch": False,
         "current_place": None,
         "chapter": None,
-        "weapon loadout": None
+        "weapon loadout": None,
+        "alive": True
     },
     "Terminator2" : {
         "action points": 4,
         "overwatch": False,
         "current_place": None,
         "chapter": None,
-        "weapon loadout": None
+        "weapon loadout": None,
+        "alive": True
     },
     "Terminator3" : {
         "action points": 4,
         "overwatch": False,
         "current_place": None,
         "chapter": None,
-        "weapon loadout": None
+        "weapon loadout": None,
+        "alive": True
     },
     "Terminator4" : {
         "action points": 4,
         "overwatch": False,
         "current_place": None,
         "chapter": None,
-        "weapon loadout": None
+        "weapon loadout": None,
+        "alive": True
     }
 }
