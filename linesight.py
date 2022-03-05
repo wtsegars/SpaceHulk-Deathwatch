60e38e01d582da115893da7ed2f4e513b17543d1
wtsegars <wtsegars@mac.com>
2022-03-04 21:52:43 -0500

    Made linesight dict into a nested dict

--- linesight.py
+++ linesight.py
@@ -1,138 +1,138 @@
 line_of_sight = {
-    "starting hall": [
-        "s1",
-        "s2",
-        "s3",
-        "s4",
-        "s5",
-        "c1"
-    ],
-    "lower hall": [
-        "g3",
-        "g4",
-        "ll5",
-        "ll4",
-        "ll3",
-        "ll2",
-        "ll1",
-        "c1",
-        "lr1",
-        "lr2",
-        "lr3",
-        "lr4",
-        "lr5",
-        "g8",
-        "g7"
-    ],
-    "lower left genestealer entrance": [
-        "g3",
-        "g2",
-        "g1"
-    ],
-    "lower right genestealer entrance": [
-        "g7",
-        "g6",
-        "g5"
-    ],
-    "left hallway": [
-        "ll5",
-        "l1",
-        "l2",
-        "l3",
-        "l4",
-        "l5",
-        "l6",
-        "l7",
-        "l8",
-        "l9",
-        "l10",
-        "l11",
-        "l12",
-        "l13"
-    ],
-    "right hallway": [
-        "lr5",
-        "r1",
-        "r2",
-        "r3",
-        "r4",
-        "r5",
-        "r6",
-        "r7",
-        "r8",
-        "r9",
-        "r10",
-        "r11",
-        "r12",
-        "r13"
-    ],
-    "center hall": [
-        "l6",
-        "cl1",
-        "cl2",
-        "cl3",
-        "cl4",
-        "c4",
-        "cr4",
-        "cr3",
-        "cr2",
-        "cr1",
-        "r6"
-    ],
-    "lower middle hall": [
-        "c2",
-        "c3",
-        "c4",
-        "c5",
-        "c6"
-    ],
-    "lower middle genestealer entrance": [
-        "g9",
-        "g10", 
-        "c2",
-        "g12",
-        "g11"
-    ],
-    "upper middle genestealer entrance": [
-        "g13",
-        "g14",
-        "c6",
-        "g16",
-        "g15"
-    ],
-    "upper middle hall": [
-        "c7",
-        "c8",
-        "c9",
-        "c10",
-        "c11",
-        "c12"
-    ],
-    "lower top genestealer entrance": [
-        "g19",
-        "g20",
-        "c7",
-        "g18",
-        "g17"
-    ],
-    "upper top genestealer entrance": [
-        "g23",
-        "g24",
-        "c12",
-        "g22",
-        "g21"
-    ],
-    "upper hall": [
-        "l13",
-        "ul1",
-        "ul2",
-        "ul3",
-        "ul4",
-        "c10",
-        "ur4",
-        "ur3",
-        "ur2",
-        "ur1",
-        "r13"
-    ]
+    "starting hall": {
+        "s1" : 0,
+        "s2" : 1,
+        "s3" : 2,
+        "s4" : 3,
+        "s5" : 4,
+        "c1" : 5
+    },
+    "lower hall": {
+        "g3" : 0,
+        "g4" : 1,
+        "ll5" : 2,
+        "ll4" : 3,
+        "ll3" : 4,
+        "ll2" : 5,
+        "ll1" : 6,
+        "c1" : 7,
+        "lr1" : 8,
+        "lr2" : 9,
+        "lr3" : 10,
+        "lr4" : 11,
+        "lr5" : 12,
+        "g8" : 13,
+        "g7" : 14
+    },
+    "lower left genestealer entrance": {
+        "g3" : 0,
+        "g2" : 1,
+        "g1" : 2
+    },
+    "lower right genestealer entrance": {
+        "g7" : 0,
+        "g6" : 1,
+        "g5" : 2
+    },
+    "left hallway": {
+        "ll5" : 0,
+        "l1" : 1,
+        "l2" : 2,
+        "l3" : 3,
+        "l4" : 4,
+        "l5" : 5,
+        "l6" : 6,
+        "l7" : 7,
+        "l8" : 8,
+        "l9" : 9,
+        "l10" : 10,
+        "l11" : 11,
+        "l12" : 12,
+        "l13" : 13
+    },
+    "right hallway": {
+        "lr5" : 0,
+        "r1" : 1,
+        "r2" : 2,
+        "r3" : 3,
+        "r4" : 4,
+        "r5" : 5,
+        "r6" : 6,
+        "r7" : 7,
+        "r8" : 8,
+        "r9" : 9,
+        "r10" : 10,
+        "r11" : 11,
+        "r12" : 12,
+        "r13" : 13
+    },
+    "center hall": {
+        "l6" : 0,
+        "cl1" : 1,
+        "cl2" : 2,
+        "cl3" : 3,
+        "cl4" : 4,
+        "c4" : 5,
+        "cr4" : 6,
+        "cr3" : 7,
+        "cr2" : 8,
+        "cr1" : 9,
+        "r6" : 10
+    },
+    "lower middle hall": {
+        "c2" : 0,
+        "c3" : 1,
+        "c4" : 2,
+        "c5" : 3,
+        "c6" : 4
+    },
+    "lower middle genestealer entrance": {
+        "g9" : 0,
+        "g10" : 1, 
+        "c2" : 2,
+        "g12" : 3,
+        "g11" : 4
+    },
+    "upper middle genestealer entrance": {
+        "g13" : 0,
+        "g14" : 1,
+        "c6" : 2,
+        "g16" : 3,
+        "g15" : 4
+    },
+    "upper middle hall": {
+        "c7" : 0,
+        "c8" : 1,
+        "c9" : 2,
+        "c10" : 3,
+        "c11" : 4,
+        "c12" : 5
+    },
+    "lower top genestealer entrance": {
+        "g19" : 0,
+        "g20" : 1,
+        "c7" : 2,
+        "g18" : 3,
+        "g17" : 4
+    },
+    "upper top genestealer entrance": {
+        "g23" : 0,
+        "g24" : 1,
+        "c12" : 2,
+        "g22" : 3,
+        "g21" : 4
+    },
+    "upper hall": {
+        "l13" : 0,
+        "ul1" : 1,
+        "ul2" : 2,
+        "ul3" : 3,
+        "ul4" : 4,
+        "c10" : 5,
+        "ur4" : 6,
+        "ur3" : 7,
+        "ur2" : 8,
+        "ur1" : 9,
+        "r13" : 10
+    }
 }
