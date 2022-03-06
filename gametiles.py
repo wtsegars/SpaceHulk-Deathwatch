11d18d47acb5ef976f337d64497dede2d350cd6f
wtsegars <wtsegars@mac.com>
2022-03-06 17:21:32 -0500

    Added ids to all gametiles within tiles dict

--- gametiles.py
+++ gametiles.py
@@ -4,7 +4,8 @@ tiles = {
         "on fire": False,
         "connected to": {
             "s2": "north"
-        }
+        },
+        "id_1" : 0
     },
     "s2": {
         "occupied": False,
@@ -12,7 +13,8 @@ tiles = {
         "connected to": {
             "s1": "south",
             "s3": "north"
-        }
+        },
+        "id_1" : 1
     },
     "s3": {
         "occupied": False,
@@ -20,7 +22,8 @@ tiles = {
         "connected to": {
             "s2": "south",
             "s4": "north"
-        }
+        },
+        "id_1" : 2
     },
     "s4": {
         "occupied": False,
@@ -28,7 +31,8 @@ tiles = {
         "connected to": {
             "s3": "south",
             "s5": "north"
-        }
+        },
+        "id_1" : 3
     },
     "s5": {
         "occupied": False,
@@ -36,7 +40,8 @@ tiles = {
         "connected to": {
             "s4": "south",
             "c1": "north"
-        }
+        },
+        "id_1" : 4
     },
     "g1": {
         "occupied": False,
@@ -44,7 +49,8 @@ tiles = {
         "entrance": True,
         "connected to": {
             "g2": "north"
-        }
+        },
+        "id_1" : 2
     },
     "g2": {
         "occupied": False,
@@ -52,7 +58,8 @@ tiles = {
         "connected to": {
             "g1": "south",
             "g3": "north"
-        }
+        },
+        "id_1" : 1
     },
     "g3": {
         "occupied": False,
@@ -60,7 +67,9 @@ tiles = {
         "connected to": {
             "g2": "south",
             "g4": "east"
-        }
+        },
+        "id_1" : 0,
+        "id_2" : 0
     },
     "g4": {
         "occupied": False,
@@ -71,7 +80,8 @@ tiles = {
         "connected to": {
             "g3": "west",
             "ll5": "east"
-        }
+        },
+        "id_1" : 1
     },
     "g5": {
         "occupied": False,
@@ -79,7 +89,8 @@ tiles = {
         "entrance": True,
         "connected to": {
             "g6": "north"
-        }
+        },
+        "id_1" : 2
     },
     "g6": {
         "occupied": False,
@@ -87,7 +98,8 @@ tiles = {
         "connected to": {
             "g5": "south",
             "g7": "north"
-        }
+        },
+        "id_1" : 1
     },
     "g7": {
         "occupied": False,
@@ -95,7 +107,9 @@ tiles = {
         "connected to": {
             "g6": "south",
             "g8": "west"
-        }
+        },
+        "id_1" : 14,
+        "id_1" : 0
     },
     "g8": {
         "occupied": False,
@@ -106,7 +120,8 @@ tiles = {
         "connected to": {
             "g7": "east",
             "lr5": "west"
-        }
+        },
+        "id_1" : 13
     },
     "g9": {
         "occupied": False,
@@ -114,7 +129,8 @@ tiles = {
         "entrance": True,
         "connected to": {
             "g10": "east"
-        }
+        },
+        "id_1" : 0
     },
     "g10": {
         "occupied": False,
@@ -122,7 +138,8 @@ tiles = {
         "connected to": {
             "g9": "west",
             "c2": "east"
-        }
+        },
+        "id_1" : 1
     },
     "g11": {
         "occupied": False,
@@ -130,7 +147,8 @@ tiles = {
         "entrance": True,
         "connected to": {
             "g12": "west"
-        }
+        },
+        "id_1" : 4
     },
     "g12": {
         "occupied": False,
@@ -138,7 +156,8 @@ tiles = {
         "connected to": {
             "g11": "east",
             "c2": "west"
-        }
+        },
+        "id_1" : 3
     },
     "g13": {
         "occupied": False,
@@ -146,7 +165,8 @@ tiles = {
         "entrance": True,
         "connected to": {
             "g14": "east"
-        }
+        },
+        "id_1" : 0
     },
     "g14": {
         "occupied": False,
@@ -154,7 +174,8 @@ tiles = {
         "connected to": {
             "g13": "west",
             "c6": "east"
-        }
+        },
+        "id_1" : 1
     },
     "g15": {
         "occupied": False,
@@ -162,7 +183,8 @@ tiles = {
         "entrance": True,
         "connected to": {
             "g16": "west"
-        }
+        },
+        "id_1" : 4
     },
     "g16": {
         "occupied": False,
@@ -170,7 +192,8 @@ tiles = {
         "connected to": {
             "g15": "east",
             "c6": "west"
-        }
+        },
+        "id_1" : 3
     },
     "g17": {
         "occupied": False,
@@ -178,7 +201,8 @@ tiles = {
         "entrance": True,
         "connected to": {
             "g18": "west"
-        }
+        },
+        "id_1" : 4
     },
     "g18": {
         "occupied": False,
@@ -189,7 +213,8 @@ tiles = {
         "connected to": {
             "g17": "east",
             "c7": "west"
-        }
+        },
+        "id_1" : 3
     },
     "g19": {
         "occupied": False,
@@ -197,7 +222,8 @@ tiles = {
         "entrance": True,
         "connected to": {
             "g20": "east"
-        }
+        },
+        "id_1" : 0
     },
     "g20": {
         "occupied": False,
@@ -208,7 +234,8 @@ tiles = {
         "connected to": {
             "g19": "west",
             "c7": "east"
-        }
+        },
+        "id_1" : 1
     },
     "g21": {
         "occupied": False,
@@ -216,7 +243,8 @@ tiles = {
         "entrance": True,
         "connected to": {
             "g22": "west"
-        }
+        },
+        "id_1" : 4
     },
     "g22": {
         "occupied": False,
@@ -227,7 +255,8 @@ tiles = {
         "connected to": {
             "g21": "east",
             "c12": "west"
-        }
+        },
+        "id_1" : 3
     },
     "g23": {
         "occupied": False,
@@ -235,7 +264,8 @@ tiles = {
         "entrance": True,
         "connected to": {
             "g24": "east"
-        }
+        },
+        "id_1" : 0
     },
     "g24": {
         "occupied": False,
@@ -246,7 +276,8 @@ tiles = {
         "connected to": {
             "g23": "west",
             "c12": "east"
-        }
+        },
+        "id_1" : 1
     },
     "c1": {
         "occupied": False,
@@ -255,7 +286,9 @@ tiles = {
             "s5": "south",
             "lr1": "east",
             "ll1": "west"
-        }
+        },
+        "id_1" : 5,
+        "id_2" : 7
     },
     "c2": {
         "occupied": False,
@@ -264,7 +297,9 @@ tiles = {
             "g10": "west",
             "g12": "east",
             "c3": "north"
-        }
+        },
+        "id_1" : 0,
+        "id_2" : 2
     },
     "c3": {
         "occupied": False,
@@ -275,7 +310,8 @@ tiles = {
         "connected to": {
             "c2": "south",
             "c4": "norht"
-        }
+        },
+        "id_1" : 1
     },
     "c4": {
         "occupied": False,
@@ -285,7 +321,9 @@ tiles = {
             "cl4": "west",
             "c5": "north",
             "cr4": "east"
-        }
+        },
+        "id_1" : 5,
+        "id_2" : 2
     },
     "c5": {
         "occupied": False,
@@ -296,7 +334,8 @@ tiles = {
         "connected to": {
             "c4": "south",
             "c6": "north"
-        }
+        },
+        "id_1" : 3
     },
     "c6": {
         "occupied": False,
@@ -305,7 +344,9 @@ tiles = {
             "c5": "south",
             "g14": "west",
             "g16": "east"
-        }
+        },
+        "id_1" : 4,
+        "id_2" : 2
     },
     "c7": {
         "occupied": False,
@@ -313,7 +354,9 @@ tiles = {
         "connected to": {
             "g18": "east",
             "g20": "west"
-        }
+        },
+        "id_1" : 0,
+        "id_2" : 2
     },
     "c8": {
         "occupied": False,
@@ -321,7 +364,8 @@ tiles = {
         "connected to": {
             "c7": "south",
             "c9": "north"
-        }
+        },
+        "id_1" : 1
     },
     "c9": {
         "occupied": False,
@@ -329,7 +373,8 @@ tiles = {
         "connected to": {
             "c8": "south",
             "c10": "north"
-        }
+        },
+        "id_1" : 2
     },
     "c10": {
         "occupied": False,
@@ -339,7 +384,9 @@ tiles = {
             "ur4": "east",
             "ul4": "west",
             "c11": "north"
-        }
+        },
+        "id_1" : 3,
+        "id_2" : 5
     },
     "c11": {
         "occupied": False,
@@ -347,7 +394,8 @@ tiles = {
         "connected to": {
             "c10": "south",
             "c12": "north"
-        }
+        },
+        "id_1" : 4
     },
     "c12": {
         "occupied": False,
@@ -356,7 +404,9 @@ tiles = {
             "c11": "south",
             "g22": "east",
             "g24": "west"
-        }
+        },
+        "id_1" : 5,
+        "id_2" : 2
     },
     "ll1": {
         "occupied": False,
@@ -364,7 +414,8 @@ tiles = {
         "connected to": {
             "c1": "east",
             "ll2": "west"
-        }
+        },
+        "id_1" : 6
     },
     "ll2": {
         "occupied": False,
@@ -372,7 +423,8 @@ tiles = {
         "connected to": {
             "ll1": "east",
             "ll3": "west"
-        }
+        },
+        "id_1" : 5
     },
     "ll3": {
         "occupied": False,
@@ -380,7 +432,8 @@ tiles = {
         "connected to": {
             "ll2": "east",
             "ll4": "west"
-        }
+        },
+        "id_1" : 4
     },
     "ll4": {
         "occupied": False,
@@ -388,7 +441,8 @@ tiles = {
         "connected to": {
             "ll3": "east",
             "ll5": "west"
-        }
+        },
+        "id_1" : 3
     },
     "ll5": {
         "occupied": False,
@@ -397,7 +451,9 @@ tiles = {
             "ll4": "east",
             "g4": "west",
             "l1": "north"
-        }
+        },
+        "id_1" : 2,
+        "id_2" : 0
     },
     "lr1": {
         "occupied": False,
@@ -405,7 +461,8 @@ tiles = {
         "connected to": {
             "c1": "west",
             "lr1": "east"
-        }
+        },
+        "id_1" : 8
     },
     "lr2": {
         "occupied": False,
@@ -413,7 +470,8 @@ tiles = {
         "connected to": {
             "lr1": "west",
             "lr3": "east"
-        }
+        },
+        "id_1" : 9
     },
     "lr3": {
         "occupied": False,
@@ -421,7 +479,8 @@ tiles = {
         "connected to": {
             "lr2": "west",
             "lr4": "east"
-        }
+        },
+        "id_1" : 10
     },
     "lr4": {
         "occupied": False,
@@ -429,7 +488,8 @@ tiles = {
         "connected to": {
             "lr3": "west",
             "lr5": "east"
-        }
+        },
+        "id_1" : 11
     },
     "lr5": {
         "occupied": False,
@@ -438,7 +498,9 @@ tiles = {
             "lr4": "west",
             "g8": "east",
             "r1": "north"
-        }
+        },
+        "id_1" : 12,
+        "id_2" : 0
     },
     "l1": {
         "occupied": False,
@@ -446,7 +508,8 @@ tiles = {
         "connected to": {
             "ll5": "south",
             "l2": "north"
-        }
+        },
+        "id_1" : 1
     },
     "l2": {
         "occupied": False,
@@ -454,7 +517,8 @@ tiles = {
         "connected to": {
             "l1": "south",
             "l3": "north"
-        }
+        },
+        "id_1" : 2
     },
     "l3": {
         "occupied": False,
@@ -462,7 +526,8 @@ tiles = {
         "connected to": {
             "l2": "south",
             "l4": "north"
-        }
+        },
+        "id_1" : 3
     },
     "l4": {
         "occupied": False,
@@ -470,7 +535,8 @@ tiles = {
         "connected to": {
             "l3": "south",
             "l5": "north"
-        }
+        },
+        "id_1" : 4
     },
     "l5": {
         "occupied": False,
@@ -478,7 +544,8 @@ tiles = {
         "connected to": {
             "l4": "south",
             "l6": "north"
-        }
+        },
+        "id_1" : 5
     },
     "l6": {
         "occupied": False,
@@ -487,7 +554,9 @@ tiles = {
             "l5": "south",
             "l7": "north",
             "cl1": "east"
-        }
+        },
+        "id_1" : 6,
+        "id_1" : 0
     },
     "l7": {
         "occupied": False,
@@ -495,7 +564,8 @@ tiles = {
         "connected to": {
             "l6": "south",
             "l8": "north"
-        }
+        },
+        "id_1" : 7
     },
     "l8": {
         "occupied": False,
@@ -503,7 +573,8 @@ tiles = {
         "connected to": {
             "l7": "south",
             "l9": "north"
-        }
+        },
+        "id_1" : 8
     },
     "l9": {
         "occupied": False,
@@ -511,7 +582,8 @@ tiles = {
         "connected to": {
             "l8": "south",
             "l10": "north"
-        }
+        },
+        "id_1" : 9
     },
     "l10": {
         "occupied": False,
@@ -519,7 +591,8 @@ tiles = {
         "connected to": {
             "l9": "south",
             "l11": "north"
-        }
+        },
+        "id_1" : 10
     },
     "l11": {
         "occupied": False,
@@ -527,7 +600,8 @@ tiles = {
         "connected to": {
             "l10": "south",
             "l12": "north"
-        }
+        },
+        "id_1" : 11
     },
     "l12": {
         "occupied": False,
@@ -535,7 +609,8 @@ tiles = {
         "connected to": {
             "l11": "south",
             "l13": "north"
-        }
+        },
+        "id_1" : 12
     },
     "l13": {
         "occupied": False,
@@ -543,7 +618,9 @@ tiles = {
         "connected to": {
             "l12": "south",
             "ul1": "east"
-        }
+        },
+        "id_1" : 13,
+        "id_2" : 0
     },
     "r1": {
         "occupied": False,
@@ -551,7 +628,8 @@ tiles = {
         "connected to": {
             "lr5": "south",
             "r2": "north"
-        }
+        },
+        "id_1" : 1
     },
     "r2": {
         "occupied": False,
@@ -559,7 +637,8 @@ tiles = {
         "connected to": {
             "r1": "south",
             "r3": "north"
-        }
+        },
+        "id_1" : 2
     },
     "r3": {
         "occupied": False,
@@ -567,7 +646,8 @@ tiles = {
         "connected to": {
             "r2": "south",
             "r4": "north"
-        }
+        },
+        "id_1" : 3
     },
     "r4": {
         "occupied": False,
@@ -575,7 +655,8 @@ tiles = {
         "connected to": {
             "r3": "south",
             "r5": "north"
-        }
+        },
+        "id_1" : 4
     },
     "r5": {
         "occupied": False,
@@ -583,7 +664,8 @@ tiles = {
         "connected to": {
             "r4": "south",
             "r6": "north"
-        }
+        },
+        "id_1" : 5
     },
     "r6": {
         "occupied": False,
@@ -592,7 +674,9 @@ tiles = {
             "r5": "south",
             "r7": "north",
             "cr1": "west"
-        }
+        },
+        "id_1" : 6,
+        "id_2" : 10
     },
     "r7": {
         "occupied": False,
@@ -600,7 +684,8 @@ tiles = {
         "connected to": {
             "r6": "south",
             "r8": "north"
-        }
+        },
+        "id_1" : 7
     },
     "r8": {
         "occupied": False,
@@ -608,7 +693,8 @@ tiles = {
         "connected to": {
             "r7": "south",
             "r9": "north"
-        }
+        },
+        "id_1" : 8
     },
     "r9": {
         "occupied": False,
@@ -616,7 +702,8 @@ tiles = {
         "connected to": {
             "r8": "south",
             "r10": "north"
-        }
+        },
+        "id_1" : 9
     },
     "r10": {
         "occupied": False,
@@ -624,7 +711,8 @@ tiles = {
         "connected to": {
             "r9": "south",
             "r11": "north"
-        }
+        },
+        "id_1" : 10
     },
     "r11": {
         "occupied": False,
@@ -632,7 +720,8 @@ tiles = {
         "connected to": {
             "r10": "south",
             "r12": "north"
-        }
+        },
+        "id_1" : 11
     },
     "r12": {
         "occupied": False,
@@ -640,7 +729,8 @@ tiles = {
         "connected to": {
             "r11": "south",
             "r13": "north"
-        }
+        },
+        "id_1" : 12
     },
     "r13": {
         "occupied": False,
@@ -648,7 +738,8 @@ tiles = {
         "connected to": {
             "r12": "south",
             "ur1": "west"
-        }
+        },
+        "id_1" : 13
     },
     "ul1": {
         "occupied": False,
@@ -656,7 +747,8 @@ tiles = {
         "connected to": {
             "l13": "west",
             "ul2": "east"
-        }
+        },
+        "id_1" : 1
     },
     "ul2": {
         "occupied": False,
@@ -664,7 +756,8 @@ tiles = {
         "connected to": {
             "ul1": "west",
             "ul3": "east"
-        }
+        },
+        "id_1" : 2
     },
     "ul3": {
         "occupied": False,
@@ -672,7 +765,8 @@ tiles = {
         "connected to": {
             "ul2": "west",
             "ul4": "east"
-        }
+        },
+        "id_1" : 3
     },
     "ul4": {
         "occupied": False,
@@ -680,7 +774,8 @@ tiles = {
         "connected to": {
             "ul3": "west",
             "c10": "east"
-        }
+        },
+        "id_1" : 4
     },
     "ur1": {
         "occupied": False,
@@ -688,7 +783,8 @@ tiles = {
         "connected to": {
             "r13": "east",
             "ur2": "west"
-        }
+        },
+        "id_1" : 9
     },
     "ur2": {
         "occupied": False,
@@ -696,7 +792,8 @@ tiles = {
         "connected to": {
             "ur1": "east",
             "ur3": "west"
-        }
+        },
+        "id_1" : 8
     },
     "ur3": {
         "occupied": False,
@@ -704,7 +801,8 @@ tiles = {
         "connected to": {
             "ur2": "east",
             "ur4": "west"
-        }
+        },
+        "id_1" : 7
     },
     "ur4": {
         "occupied": False,
@@ -712,7 +810,8 @@ tiles = {
         "connected to": {
             "ur3": "east",
             "c10": "west"
-        }
+        },
+        "id_1" : 6
     },
     "cl1": {
         "occupied": False,
@@ -720,7 +819,8 @@ tiles = {
         "connected to": {
             "l6": "west",
             "cl2": "east"
-        }
+        },
+        "id_1" : 1
     },
     "cl2": {
         "occupied": False,
@@ -728,7 +828,8 @@ tiles = {
         "connected to": {
             "cl1": "west",
             "cl3": "east"
-        }
+        },
+        "id_1" : 2
     },
     "cl3": {
         "occupied": False,
@@ -736,7 +837,8 @@ tiles = {
         "connected to": {
             "cl2": "west",
             "cl4": "east"
-        }
+        },
+        "id_1" : 3
     },
     "cl4": {
         "occupied": False,
@@ -744,7 +846,8 @@ tiles = {
         "connected to": {
             "cl3": "west",
             "c4": "east"
-        }
+        },
+        "id_1" : 4
     },
     "cr1": {
         "occupied": False,
@@ -752,7 +855,8 @@ tiles = {
         "connected to": {
             "r6": "east",
             "cr2": "west"
-        }
+        },
+        "id_1" : 9
     },
     "cr2": {
         "occupied": False,
@@ -760,7 +864,8 @@ tiles = {
         "connected to": {
             "cr1": "east",
             "cr3": "west"
-        }
+        },
+        "id_1" : 8
     },
     "cr3": {
         "occupied": False,
@@ -768,7 +873,8 @@ tiles = {
         "connected to": {
             "cr2": "east",
             "cr4": "west"
-        }
+        },
+        "id_1" : 7
     },
     "cr4": {
         "occupied": False,
@@ -776,6 +882,7 @@ tiles = {
         "connected to": {
             "cr3": "east",
             "c4": "west"
-        }
+        },
+        "id_1" : 6
     }
 }
